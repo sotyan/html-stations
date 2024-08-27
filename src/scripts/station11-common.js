5a92bb626f138c177512c1bc2958da33a1fdbf27
sotyan <happy.tanbarin2@gmail.com>
2024-08-27 15:19:12 +0900

    Station12,complete

--- src/scripts/station11-common.js
+++ src/scripts/station11-common.js
@@ -1,3 +1,3 @@
 var result = getData();
-var elem = document.getElementById("result");
-elem.innerHTML = result;
+var elem = document.getElementById("result"); // htmlのresultIDのところ
+elem.innerHTML = result; // 上でとったresultIDのところにresultが代入される
